7277230dfe9112231c62985164e71d3f24807ae3
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-05-14 14:02:30 +0100

    hm

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,8 +2,6 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { Card, Title, Text, Grid, Col } from "@tremor/react";
 
-import { api } from "~/utils/api";
-
 const Home: NextPage = () => {
   // const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
@@ -14,14 +12,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Pokemon Classifier" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Title>Dashboard</Title>
-        <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+      <main className="bg-gray-950 p-10">
+        <Title>Pokemon Classifier - Lakee Sivaraya</Title>
+        <Text>Upload Image of Pokemon Below</Text>
 
-        <Grid numColsLg={6} className="mt-6 gap-6">
+        <Grid numColsLg={6} className="mt-6 gap-6 ">
           {/* Main section */}
-          <Col numColSpanLg={4}>
-            <Card className="h-full">
+          <Col numColSpanLg={3}>
+            <Card className="h-full border-gray-800 bg-gray-900">
               <div className="h-60">
                 <input
                   className="block w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:placeholder-gray-400"
@@ -33,12 +31,8 @@ const Home: NextPage = () => {
           </Col>
 
           {/* KPI sidebar */}
-          <Col numColSpanLg={2}>
+          <Col numColSpanLg={3}>
             <div className="space-y-6">
-              <Card>
-                <div className="h-24" />
-                Hello???
-              </Card>
               <Card>
                 <div className="h-24" />
                 Hello???
